c28434f149d14790158ae4e360dcd485096bd939
Eugene Orlov <evgeniiorlov204@gmail.com>
2021-07-16 17:05:08 +0300

    Implemented login and password validation

--- app/src/main/java/com/shpp/eorlov/assignment1/AuthActivity.kt
+++ app/src/main/java/com/shpp/eorlov/assignment1/AuthActivity.kt
@@ -2,29 +2,84 @@ package com.shpp.eorlov.assignment1
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import android.view.View
-import android.widget.CheckBox
-import android.widget.EditText
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 class AuthActivity : AppCompatActivity() {
 
+    var emailErrorMessage: TextInputLayout? = null
+    var emailField: TextInputEditText? = null
+
+    var passwordErrorMessage: TextInputLayout? = null
+    var passwordField: TextInputEditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        val intent = intent
+        initializeData()
+    }
 
-        if (intent.getStringExtra("username") != null) {
-            val usernameField = findViewById<EditText>(R.id.edit_text_email)
-            val passwordField = findViewById<EditText>(R.id.edit_text_password)
-            val username = intent.getStringExtra("username")
-            val password = intent.getStringExtra("password")
-            usernameField.setText(username)
-            passwordField.setText(password)
+    /**
+     * Initialized objects that contains information about email and password
+     */
+    private fun initializeData() {
+        emailErrorMessage = findViewById(R.id.text_input_layout_email)
+        emailField = findViewById(R.id.text_input_edit_text_email)
+        emailField?.addTextChangedListener(ValidationTextWatcher(emailField!!))
+
+        passwordErrorMessage = findViewById(R.id.text_input_layout_password)
+        passwordField = findViewById(R.id.text_input_edit_text_password)
+        passwordField?.addTextChangedListener(ValidationTextWatcher(passwordField!!))
+    }
+
+    /**
+     * Specify an explicit soft input mode to use for the window,
+     * as per WindowManager.LayoutParams.softInputMode.
+     */
+    private fun requestFocus(view: View) {
+        if (view.requestFocus()) {
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
         }
+    }
+
+    private fun validatePassword(): Boolean {
+        if (passwordField!!.text.toString().trim { it <= ' ' }.isEmpty()) {
+            passwordErrorMessage!!.error = "Password is required"
+            requestFocus(passwordField!!)
+            return false
+        } else if (passwordField!!.text.toString().length < 6) {
+            passwordErrorMessage!!.error = "Password can't be less than 6 digit"
+            requestFocus(passwordField!!)
+            return false
+        } else {
+            passwordErrorMessage!!.isErrorEnabled = false
+        }
+        return true
+    }
+
+    fun validateEmail(): Boolean {
+        if (emailField!!.text.toString().trim { it <= ' ' }.isEmpty()) {
+            emailErrorMessage!!.isErrorEnabled = false
+        } else {
+            val emailId = emailField!!.text.toString()
+            val isValid = Patterns.EMAIL_ADDRESS.matcher(emailId).matches()
+            if (!isValid) {
+                emailErrorMessage!!.error = "Invalid Email address, ex: abc@example.com"
+                requestFocus(emailField!!)
+                return false
+            } else {
+                emailErrorMessage!!.isErrorEnabled = false
+            }
+        }
+        return true
     }
 
 
@@ -32,29 +87,29 @@ class AuthActivity : AppCompatActivity() {
      * Change current activity to MainActivity
      */
     fun goToMainActivity(view: View) {
-        val usernameField = findViewById<EditText>(R.id.edit_text_email)
-        val passwordField = findViewById<EditText>(R.id.edit_text_password)
-        var username = usernameField.text.toString()
-        var password = passwordField.text.toString()
 
-
-        val rememberMe = findViewById<View>(R.id.check_box_remember_me) as CheckBox
-        if (!rememberMe.isChecked) {
-            username = ""
-            password = ""
+        if (!validateEmail() || !validatePassword()) {
+            return
         }
 
-        startActivity(getDataToSend(username, password))
+        startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
 
+
     /**
-     * Returns data to be send to another activity
+     * Class to keep track of each character of input text fields
+     * for email and password validation
      */
-    private fun getDataToSend(username: String?, password: String?): Intent {
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra("username", username)
-        intent.putExtra("password", password)
-        return intent
+    inner class ValidationTextWatcher constructor(private val view: View) :
+        TextWatcher {
+        override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+        override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+        override fun afterTextChanged(editable: Editable) {
+            when (view.id) {
+                R.id.text_input_edit_text_email -> validateEmail()
+                R.id.text_input_edit_text_password -> validatePassword()
+            }
+        }
     }
 }
--- app/src/main/java/com/shpp/eorlov/assignment1/MainActivity.kt
+++ app/src/main/java/com/shpp/eorlov/assignment1/MainActivity.kt
@@ -11,20 +11,6 @@ import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
-    var username: String? = null
-        set(value) {
-            field = value
-        }
-        get() {
-            return field
-        }
-    private var password: String? = null
-        set(value) {
-            field = value
-        }
-        get() {
-            return field
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -35,32 +21,14 @@ class MainActivity : AppCompatActivity() {
             .circleCrop()
             .into(findViewById(R.id.image_view_person_image))
 
-        val intent = intent
-        username = intent.getStringExtra("username")
-        password = intent.getStringExtra("password")
 
-        if (username != "" && username != null) {
-            val messageText = findViewById<View>(R.id.text_view_person_name) as TextView
-            messageText.text = username
-        }
     }
 
     /**
      * Change current activity to AuthActivity
      */
     fun goToAuthActivity(view: View) {
-        startActivity(getDataToSend(username, password))
+        startActivity(Intent(this, AuthActivity::class.java))
         finish()
-    }
-
-
-    /**
-     * Returns data to be send to another activity
-     */
-    private fun getDataToSend(username: String?, password: String?): Intent {
-        val intent = Intent(this, AuthActivity::class.java)
-        intent.putExtra("username", username)
-        intent.putExtra("password", password)
-        return intent
     }
 }
