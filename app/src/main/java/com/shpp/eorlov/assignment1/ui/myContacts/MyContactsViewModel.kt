c3a3717072944c8ee2a3d9e0e64ab38114e2fb8f
Eugene <evgeniiorlov204@gmail.com>
2021-10-10 09:02:08 +0300

    Fixed bugs in code. Implemented todos.

--- app/src/main/java/com/shpp/eorlov/assignment1/ui/myContacts/MyContactsViewModel.kt
+++ app/src/main/java/com/shpp/eorlov/assignment1/ui/myContacts/MyContactsViewModel.kt
@@ -1,68 +1,37 @@
 package com.shpp.eorlov.assignment1.ui.myContacts
 
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.shpp.eorlov.assignment1.data.storage.SharedPreferencesStorageImpl
-import com.shpp.eorlov.assignment1.model.DataUsers
-import com.shpp.eorlov.assignment1.model.ResponseModel
+import com.shpp.eorlov.assignment1.base.BaseViewModel
 import com.shpp.eorlov.assignment1.model.UserModel
-import com.shpp.eorlov.assignment1.repository.MainRepositoryImpl
 import com.shpp.eorlov.assignment1.repository.UserRepositoryImpl
-import com.shpp.eorlov.assignment1.utils.Constants
 import com.shpp.eorlov.assignment1.utils.Results
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import retrofit2.HttpException
-import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
 class MyContactsViewModel @Inject constructor(
-    private val storage: SharedPreferencesStorageImpl,
-    private val userRepository: UserRepositoryImpl,
-    private val repository: MainRepositoryImpl
-) : ViewModel() {
+    private val userRepository: UserRepositoryImpl
+) : BaseViewModel() {
 
-    //todo migrate to set
     val contactsLiveData = MutableLiveData<MutableList<UserModel>>(ArrayList())
-    val usersLiveData = MutableLiveData<MutableList<UserModel>>(ArrayList())
-    val loadEventLiveData = MutableLiveData<Results>()
-    val selectedEventLiveData = MutableLiveData(false)
-    val allUsersLiveData = MutableLiveData<ResponseModel<DataUsers>>()
 
     init {
-        viewModelScope.launch {
-            if (contactsLiveData.value == null) {
-                loadEventLiveData.value = Results.INITIALIZE_DATA_ERROR
-            } else {
-                val users = userRepository.getAll().toMutableList()
-                if (users.isNotEmpty()) {
-                    contactsLiveData.value = users
-                } else {
-                    loadEventLiveData.value = Results.INITIALIZE_DATA_ERROR
-                }
-            }
+        if (contactsLiveData.value == null) {
+            loadEventLiveData.value = Results.INITIALIZE_DATA_ERROR
+        } else {
+            refreshContactsList()
         }
     }
 
-    fun getAllUsers(accessToken: String) {
+    fun refreshContactsList() {
         viewModelScope.launch {
-            loadEventLiveData.value = Results.LOADING
-            val response = try {
-                repository.getAllUsers(accessToken = "Bearer $accessToken")
-            } catch (e: IOException) {
-                loadEventLiveData.value = Results.INTERNET_ERROR
-                return@launch
-            } catch (e: HttpException) {
-                loadEventLiveData.value = Results.UNEXPECTED_RESPONSE
-                return@launch
-            }
-
-            if (response.isSuccessful && response.body() != null) {
-                allUsersLiveData.postValue(response.body()!!)
+            val users = userRepository.getAll().toMutableList()
+            if (users.isNotEmpty()) {
+                contactsLiveData.value = users
             } else {
-                loadEventLiveData.value = Results.NOT_SUCCESSFUL_RESPONSE
+                loadEventLiveData.value = Results.INITIALIZE_DATA_ERROR
             }
         }
     }
@@ -80,18 +49,23 @@ class MyContactsViewModel @Inject constructor(
      */
     fun removeItem(position: Int) {
         viewModelScope.launch {
-            contactsLiveData.value?.get(position)?.let { userRepository.delete(it) }
+            contactsLiveData.value?.get(position)?.let {
+                userRepository.delete(it.id)
+
+                contactsLiveData.value?.removeAt(position)
+                contactsLiveData.value = contactsLiveData.value
+            }
         }
-        contactsLiveData.value?.removeAt(position)
-        contactsLiveData.value = contactsLiveData.value
     }
 
     fun removeItem(item: UserModel?) {
         viewModelScope.launch {
-            item?.let { userRepository.delete(it) }
+            item?.let {
+                userRepository.delete(it.id)
+                contactsLiveData.value?.remove(item)
+                contactsLiveData.value = contactsLiveData.value
+            }
         }
-        contactsLiveData.value?.remove(item)
-        contactsLiveData.value = contactsLiveData.value
     }
 
     /**
@@ -102,9 +76,9 @@ class MyContactsViewModel @Inject constructor(
             if (!it.contains(addedItem)) {
                 viewModelScope.launch {
                     userRepository.insertAll(addedItem)
+                    contactsLiveData.value?.add(position, addedItem)
+                    contactsLiveData.value = contactsLiveData.value
                 }
-                contactsLiveData.value?.add(position, addedItem)
-                contactsLiveData.value = contactsLiveData.value
             }
         }
     }
@@ -117,31 +91,9 @@ class MyContactsViewModel @Inject constructor(
             if (!it.contains(addedItem)) {
                 viewModelScope.launch {
                     userRepository.insertAll(addedItem)
+                    contactsLiveData.value?.add(addedItem)
+                    contactsLiveData.value = contactsLiveData.value
                 }
-                contactsLiveData.value?.add(addedItem)
-                contactsLiveData.value = contactsLiveData.value
-            }
-        }
-    }
-
-    fun fetchToken(): String {
-        return storage.getString(Constants.ACCESS_TOKEN) ?: ""
-    }
-
-    fun clearContactsList() {
-        contactsLiveData.value = ArrayList()
-    }
-
-    fun addItems(addedItems: MutableList<UserModel>) {
-        viewModelScope.launch {
-            contactsLiveData.value?.let {
-                for (item in addedItems) {
-                    if (!it.contains(item)) {
-                        userRepository.insertAll(item)
-                        loadEventLiveData.value = Results.LOADING
-                    }
-                }
-                contactsLiveData.value = userRepository.getAll().toMutableList()
             }
         }
     }
--- app/src/main/java/com/shpp/eorlov/assignment1/ui/myContacts/adapter/viewHolder/ContactsViewHolder.kt
+++ app/src/main/java/com/shpp/eorlov/assignment1/ui/myContacts/adapter/viewHolder/ContactsViewHolder.kt
@@ -3,18 +3,16 @@ package com.shpp.eorlov.assignment1.ui.myContacts.adapter.viewHolder
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.shpp.eorlov.assignment1.R
-import com.shpp.eorlov.assignment1.databinding.ContactListItemBinding
+import com.shpp.eorlov.assignment1.databinding.LayoutContactListItemBinding
 import com.shpp.eorlov.assignment1.model.UserModel
 import com.shpp.eorlov.assignment1.ui.myContacts.adapter.listeners.ContactClickListener
 import com.shpp.eorlov.assignment1.utils.ext.clickWithDebounce
 
-//todo off navigation to detail view and any other fragments
 class ContactsViewHolder(
-    private val binding: ContactListItemBinding,
-    private val onContactClickListener: ContactClickListener,
+    private val binding: LayoutContactListItemBinding,
+    private val contactClickListener: ContactClickListener,
     private val multiSelect: Boolean,
-    private val selectedItems: ArrayList<UserModel>,
-    private val addContactsState: Boolean
+    private val selectedItems: ArrayList<UserModel>
 ) :
     RecyclerView.ViewHolder(binding.root) {
 
@@ -26,21 +24,15 @@ class ContactsViewHolder(
         contact.apply {
             if (multiSelect) {
                 binding.constraintLayoutContactListItem.setBackgroundResource(R.drawable.round_view_holder_selected)
-                binding.checkBoxSelectedState.visibility = View.VISIBLE
                 binding.imageViewRemoveButton.visibility = View.GONE
+                binding.checkBoxSelectedState.visibility = View.VISIBLE
                 if (selectedItems.contains(contact)) {
                     binding.checkBoxSelectedState.isChecked = true
                 }
             } else {
                 binding.constraintLayoutContactListItem.setBackgroundResource(R.drawable.round_view_holder)
                 binding.checkBoxSelectedState.visibility = View.GONE
-
-                if (addContactsState) {
-                    binding.imageViewRemoveButton.visibility = View.GONE
-                    showAddContactsUI()
-                } else {
-                    binding.imageViewRemoveButton.visibility = View.VISIBLE
-                }
+                binding.imageViewRemoveButton.visibility = View.VISIBLE
             }
 
             binding.textViewPersonName.text = name
@@ -51,85 +43,38 @@ class ContactsViewHolder(
         setListeners()
     }
 
-    private fun showAddedUserUI() {
-        binding.imageViewAddContact.visibility = View.GONE
-        binding.textViewAddContact.visibility = View.GONE
-        binding.imageViewCheckedContact.visibility = View.VISIBLE
-    }
-
-    private fun showReadyToAddUserUI() {
-        binding.imageViewAddContact.visibility = View.VISIBLE
-        binding.textViewAddContact.visibility = View.VISIBLE
-        binding.imageViewCheckedContact.visibility = View.GONE
-    }
-
-    private fun showAddContactsUI() {
-        if (!selectedItems.contains(contact)) {
-            showReadyToAddUserUI()
-        } else {
-            showAddedUserUI()
-        }
-    }
-
-
     private fun setListeners() {
         binding.apply {
             imageViewRemoveButton.clickWithDebounce {
-                onContactClickListener.onContactRemove(bindingAdapterPosition)
+                contactClickListener.onContactRemove(bindingAdapterPosition)
             }
 
             constraintLayoutContactListItem.clickWithDebounce {
-                if (!addContactsState) {
-                    if (!multiSelect) {
-                        onContactClickListener.onContactClick(contact)
-                    } else {
-                        checkBoxSelectedState.isChecked = !checkBoxSelectedState.isChecked
-                        selectItem(contact)
-                        onContactClickListener.onContactSelectedStateChanged()
-                    }
+                if (!multiSelect) {
+                    contactClickListener.onContactClick(contact)
+                } else {
+                    checkBoxSelectedState.isChecked = !checkBoxSelectedState.isChecked
+                    selectItemToRemove(contact)
+                    contactClickListener.onContactSelectedStateChanged()
                 }
             }
 
             checkBoxSelectedState.setOnClickListener {
-                selectItem(contact)
-                onContactClickListener.onContactSelectedStateChanged()
+                selectItemToRemove(contact)
+                contactClickListener.onContactSelectedStateChanged()
             }
 
             constraintLayoutContactListItem.setOnLongClickListener {
-                if (!addContactsState && !multiSelect) {
-                    onContactClickListener.onMultiselectActivated()
-                    selectItem(contact)
+                if (!multiSelect) {
+                    contactClickListener.onMultiSelectEnabled()
+                    selectItemToRemove(contact)
                 }
                 return@setOnLongClickListener true
             }
-
-            imageViewAddContact.setOnClickListener {
-                if (imageViewCheckedContact.visibility != View.VISIBLE) {
-                    activateCheckedUserInAddContact()
-                }
-            }
-
-            textViewAddContact.setOnClickListener {
-                if (imageViewCheckedContact.visibility != View.VISIBLE) {
-                    activateCheckedUserInAddContact()
-                }
-            }
         }
     }
 
-    private fun activateCheckedUserInAddContact() {
-        if (!selectedItems.contains(contact)) {
-            selectedItems.add(contact)
-            showAddedUserUI()
-
-            if (selectedItems.size == 1) {
-                onContactClickListener.onCheckedContactActivated()
-            }
-        }
-    }
-
-    // helper function that adds/removes an item to the list depending on the app's state
-    private fun selectItem(userModel: UserModel) {
+    private fun selectItemToRemove(userModel: UserModel) {
         if (selectedItems.contains(userModel)) {
             selectedItems.remove(userModel)
             binding.checkBoxSelectedState.isChecked = false
